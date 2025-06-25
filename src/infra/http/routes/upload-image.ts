1f8a8872ace4fe3fb765897a796c920a6b670865
Bruno Fernandes Horn <b.fernandeshorn@gmail.com>
2025-06-24 23:13:49 -0300

    feat: add uuidv7

--- src/infra/http/routes/upload-image.ts
+++ src/infra/http/routes/upload-image.ts
@@ -1,5 +1,7 @@
 import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
 import { z } from 'zod'
+import { db } from '@/infra/db'
+import { schema } from '@/infra/db/schemas'
 
 export const uploadImageRoute: FastifyPluginAsyncZod = async server => {
   server.post(
@@ -20,6 +22,11 @@ export const uploadImageRoute: FastifyPluginAsyncZod = async server => {
       },
     },
     async (_request, reply) => {
+      await db.insert(schema.uploads).values({
+        name: 'teste.jpg',
+        remoteKey: 'teste.jpg',
+        remoteUrl: 'http://asasa.com',
+      })
       return reply.status(201).send({ uploadId: 'test' })
     }
   )
